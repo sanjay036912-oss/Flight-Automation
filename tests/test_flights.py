679e6e1bd2fd840901713cf754def12d14a7a6f2
Sanjay G <sanjay036912@gmail.com>
2026-04-13 14:19:20 +0530

    Added GitHub Actions CI pipeline

--- tests/test_flights.py
+++ tests/test_flights.py
@@ -9,7 +9,7 @@ logger = get_logger()
 @pytest.fixture
 def browser():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         yield browser
         browser.close()
 
